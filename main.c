3914254a2690314d4bfe01e28f6a5ada06986595
paulovictorsl9 <60369624+paulovictorsl9@users.noreply.github.com>
2021-12-30 15:37:54 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main()
 {
-//variáveis
-double a, b, c, x1, x2, delta;
+    int x, y;
 
-printf("Digite o coeficiente A: ");
-scanf("%lf", &a);
+    printf("Digite dois numeros:\n");
+    scanf("%d", &x);
+    scanf("%d", &y);
 
-printf("Digite o coeficiente B: ");
-scanf("%lf", &b);
+    while (x != y){
+        if (x < y){
+            printf("crescente\n");
+        }
+        else{
+            printf("decrescente\n");
+        }
 
-printf("Digite o coeficiente C: ");
-scanf("%lf", &c);
-
-delta = (b * b) - 4 * (a * c);
-
-if (a == 0 || delta < 0){
-    printf("Essa equaçao nao possui raizes reais.\n");
-}
-else{
-    x1 = (-b + sqrt(delta)) / (2 * a);
-    x2 = (-b - sqrt(delta)) / (2 * a);
-
-    printf("x1 = %.4lf\n", x1);
-    printf("x2 = %.4lf\n", x2);
-
-}
+        printf("Digite outros dois numeros:\n");
+        scanf("%d", &x);
+        scanf("%d", &y);
+    }
 
     return 0;
 }
